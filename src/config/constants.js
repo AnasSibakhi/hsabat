8f2fe522a5951c6196230041ba802bf8714803f3
Anas Sibakhi <120749951+AnasSibakhi@users.noreply.github.com>
2026-06-16 20:59:24 +0300

    fix: restore supabaseServiceKey in constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -6,7 +6,7 @@
 export const CONFIG = Object.freeze({
   supabaseUrl:        'https://omtrbatypecsraettrbw.supabase.co',
   supabaseAnonKey:    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tdHJiYXR5cGVjc3JhZXR0cmJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MTUzNTIsImV4cCI6MjA5NjA5MTM1Mn0.71cyb5LBkCJTX1fcVNCuD8u28waSI4fWeai4ccW-asU',
-  supabaseServiceKey: '', // مخزن في Vercel Environment Variables فقط
+  supabaseServiceKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tdHJiYXR5cGVjc3JhZXR0cmJ3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDUxNTM1MiwiZXhwIjoyMDk2MDkxMzUyfQ.Oklmu9ht2PEA02d0Qy-oS-UXl0Oa4wf560BqzxwyC04',
   debtLateDays:       2,
   lowStockDefault:    10,
   lowStockThreshold:  20,
